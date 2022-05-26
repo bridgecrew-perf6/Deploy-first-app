c3a7fdef00e3a582c65caa19c745162d441c510c
hariramani <hr951414@dal.ca>
2022-05-26 11:47:12 -0300

    Deploying demo app

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+
+function App() {
+  return (
+    <div className="App" align="center">
+     
+        <h1> Hello World</h1>
+        <h2>Welcome to CSCI 5709: Advanced Web Services</h2> 
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React documentation
+        </a>
+
+        <div style={{ borderTop: "20px solid beige ", marginLeft: 10, marginRight: 10 }}> </div>
+
+        <div style={{ borderTop: "8px solid #72d5cd ", marginLeft: 2, marginRight: 2 }}></div>
+
+        <div>
+          <h3>This tutorial has been completed by:</h3>
+          <h3> Harsh Hariramani</h3>
+          <p> Banner ID : <b> B00899363</b> </p>     
+        </div>
+    
+    </div>
+  );
+}
+
+export default App;
